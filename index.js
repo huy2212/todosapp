ba1e654942bac53c03ff240432bc6850556fc32b
huyonthecloud <dsnqhuy@gmail.com>
2025-06-03 11:15:20 +0700

    Basic API

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/todos', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Todo = mongoose.model('Todo', {
+  text: String,
+});
+
+app.get('/api/todos', async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post('/api/todos', async (req, res) => {
+  const todo = new Todo({ text: req.body.text });
+  await todo.save();
+  res.json(todo);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
